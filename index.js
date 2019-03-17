c3bd95fa181346649c4833df21581842d663f3ac
Bao Pham <ryanvuepham@gmail.com>
2019-03-17 00:36:20 -0500

    delete working. finished MVP

--- index.js
+++ index.js
@@ -71,18 +71,36 @@ server.put('/api/zoos/:id', async (req, res) => {
     const count = await db('zoos').where({id: req.params.id}).update(req.body)
     
     if (count > 0) {
-      const zoo = await db("zoos").where({id: req.params.id}).first()
-      res.status(200).json(count) // or .json(zoo), depending on you
+     const zoo = await db('zoos').where({id: req.params.id}).first()
+      res.status(200).json(zoo) // or .json(count), depending on you
     } else {
       res.status(404).json({message: 'Record not found' })
     }
   } catch(error) {
-    res.status(500).json()
+    res.status(500).json(error)
   }
   
 })
 
-const port = 3300;
+// Remove zoos animal
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos').where({id: req.params.id}).del()
+    
+    if (count > 0) {
+    
+      res.status(204).end()
+    } else {
+      res.status(404).json({message: 'Record not found' })
+    }
+  } catch(error) {
+    res.status(500).json(error)
+  }
+  
+})
+
+const port = 3300
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
 })
